5f9f45361794b1ee0f772c18842059c3c44ec0f9
raindropcity <raindropcity0209@gmail.com>
2023-06-09 14:53:11 +0800

    Added comic books controller

--- src/comicBookGallery/comicBookGallery/Controllers/HomeController.cs
+++ src/comicBookGallery/comicBookGallery/Controllers/HomeController.cs
@@ -4,29 +4,8 @@ using System.Diagnostics;
 
 namespace comicBookGallery.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public class ComicBookControllers : Controller
         {
-            _logger = logger;
+            
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
 }
